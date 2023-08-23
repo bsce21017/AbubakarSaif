d8d43a897a1c0d53288a4f8e3d4efd0b2bdb78f0
bsce21017 <bsce21017@itu.edu.pk>
2023-08-23 23:33:40 +0500

    Code Pushed

---
+++ src/Main.js
@@ -0,0 +1,42 @@
+import React from "react"
+
+function About() {
+    return (
+        <div>
+            <h2>About</h2>
+            <p className="content">My name is Muhammad Abubakar Saif. I am a junior front end developer. I am currently learning React.js and I am looking for a job.</p>
+        </div>
+    )
+}
+
+function Interests() {
+    return (
+        <div>
+            <h2>Interests</h2>
+            <p className="content">I am interested in learning new things and I am also interested in playing video games. I am also interested in learning about new technologies. </p>
+        </div>
+    )
+}
+
+function Footer() {
+    return (
+        <footer>
+            <div className="footer-flex"> 
+                <img className="icon" src = {process.env.PUBLIC_URL + "/Facebook Icon.svg"} alt="Fb Icon" />
+                <img className="icon" src = {process.env.PUBLIC_URL + "/Instagram Icon.png"} alt="Insta Icon" />
+                <a href="https://github.com/bsce21017"><img className="icon" src = {process.env.PUBLIC_URL + "/GitHub Icon.svg"} alt="GitHub Icon" /></a>
+                <img className="icon" src = {process.env.PUBLIC_URL + "/Twitter Icon.svg"} alt="Twitter Icon" />
+            </div>
+        </footer>
+    )
+}
+export default function Main() {
+    return (
+        <div>
+            <About />
+            <Interests />
+            <Footer />
+        </div>
+    )
+}
+
---
+++ src/Navbar.js
@@ -0,0 +1,31 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+function Picture() {
+    return (
+        <div>
+            <img className = "header-image" src= {process.env.PUBLIC_URL + "/Abubakar.jpg"} alt="Abubakar" />
+        </div>
+    )
+}
+
+function ButtonFlex() {
+    return (
+        <div className = "button-flex">
+            <a href="mailto:eagleofpak47@gmail.com"><button className = "email-button" >✉ Email</button></a>
+            <a href="https://www.linkedin.com/in/muhammad-abubakar-saif-a4540822a/"><button className = "lkd-button">LinkedIn</button></a>
+        </div>
+    )
+}
+
+export default function Navbar() {
+    return (
+        <div>
+            <Picture />
+            <h1>Muhammad Abubakar Saif</h1>
+            <h2 className= "caption">Junior Front End Developer</h2>
+            <p className= "email">eagleofpak47@gmail.com</p>
+            <ButtonFlex />
+        </div>
+    )   
+    }
